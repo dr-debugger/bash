5576ed461390c58d9e04f3afe490d9e218d8fadd
arijit <arijitmaiti@technoexponent.com>
2022-09-12 19:46:47 +0530

    files

---
+++ basic/basic.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "hello world"
+
+#✡️Redirect to file
+: ' one > symbol means, copy the user input and place it to the respected file
+
+if you wanna append something with existing file than use double >> symbol.'
+cat > file.txt
+echo "Now append something with previous text and press Ctrl + d"
+cat >> file.txt
---
+++ practiceWfile.sh
@@ -0,0 +1,7 @@
+# Create a new dir
+mkdir basic
+# move existing file to that directory
+mv basic.sh ./basic
+
+# delete file
+rm -rf file.txt
